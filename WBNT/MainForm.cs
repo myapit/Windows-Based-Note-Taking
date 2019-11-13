131aab90cad15b4be97e0d0467ac21d997816f9d
myapit <hafeez.cs@gmail.com>
2019-11-13 12:09:57 +0800

    init development soution using sharpdevelop

---
+++ WBNT/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: domokun
+ * Date: 13/11/2019
+ * Time: 11:33 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WBNT
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
